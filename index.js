6a56a62495adb9d782131248fbccc9360e52a256
Kirill <dvcarry@gmail.com>
2021-02-22 14:47:23 +0300

    delete dotenv

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-require('dotenv').config()
+// require('dotenv').config()
 
 
 const tasks = require('./routes/tasks')
